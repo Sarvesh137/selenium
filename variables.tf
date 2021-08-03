fe6bd8e31f7189cbbddbe17541ba6f3db6a6b677
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-03 14:51:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "Jenkins packaged by Bitnami-2-289-3-0 on Debian 10-AutogenByAWSMP-"
+  default     = "Default"
 }
 
 variable "tag_name" {
