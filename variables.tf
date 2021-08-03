2d4cbb97e4bfc4133c50f2f8ee213711bffe7cad
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-03 15:14:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "default"
+  default     = "selenium"
 }
 
 variable "tag_name" {
