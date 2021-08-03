975545503e13d93e8778b113968fc7991446f8c6
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-03 14:55:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "sg-058a1b7e6f35c0381"
+  default     = "Selenium_SG"
 }
 
 variable "tag_name" {
