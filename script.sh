0f58a592f804b7d91e892182e2edb9e75bbaebff
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-04 12:09:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sudo sed -i 's!fd:// --containerd=/run/containerd/containerd.sock $OPTIONS $DOCKER_STORAGE_OPTIONS $DOCKER_ADD_RUNTIMES! tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock!g' /lib/systemd/system/docker.service ;
-sudo systemctl daemon-reload ;
-sudo service docker restart
+sed -i 's!fd:// --containerd=/run/containerd/containerd.sock $OPTIONS $DOCKER_STORAGE_OPTIONS $DOCKER_ADD_RUNTIMES! tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock!g' /lib/systemd/system/docker.service ;
+systemctl daemon-reload ;
+service docker restart
