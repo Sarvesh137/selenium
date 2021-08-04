a7fc8d2c9c08f63f24bc3c8b81edf50029d49a95
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-04 12:03:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
+//  subnet_id = var.subnet_id
   security_groups= [var.security_group]
 //  vpc_security_group_ids= [var.security_group]
   tags= {
