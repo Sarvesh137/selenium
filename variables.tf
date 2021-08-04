42d1ab9dcfd2fce32595ceb59166c67f38654e66
Sarvesh137 <sarveshpatil1307@gmail.com>
2021-08-04 12:00:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "Selenium"
+  default     = "my-jenkins-security-group"
 }
 
 variable "subnet_id" {
